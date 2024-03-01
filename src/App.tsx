441e8b575056e569ae2bda591f66e0821a4493ee
samuelmevik <samuel.mevik@outlook.com>
2024-03-01 11:07:03 +0100

    Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { signal } from "@preact/signals-react";
 const counter = signal<number>(0);
 function App() {
   return (
-    <div>
+    <div className="mx-auto flex max-w-3xl flex-col">
       <h1 className="bg-blue-200">Counter</h1>
       <button onClick={() => (counter.value += 1)}>Increment</button>
       <button onClick={() => (counter.value -= 1)}>Decrement</button>
