96fade750a155f6e9d6cad89235ed07f6ef301f9
samuelmevik <samuel.mevik@outlook.com>
2024-03-01 11:07:53 +0100

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="flex h-14 items-center justify-between px-2 py-4">
+      <h1 className="hidden text-xl sm:inline">Sorting Visualizer</h1>
+      <div className="flex-1"></div>
+    </header>
+  );
+}
+export default Header;
