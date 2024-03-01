d67d58b066e87cbc6d1693901be701382627941f
samuelmevik <samuel.mevik@outlook.com>
2024-03-01 10:34:44 +0100

    project init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { signal } from "@preact/signals-react";
+
+const counter = signal<number>(0);
+function App() {
+  return (
+    <div>
+      <h1 className="bg-blue-200">Counter</h1>
+      <button onClick={() => (counter.value += 1)}>Increment</button>
+      <button onClick={() => (counter.value -= 1)}>Decrement</button>
+      <p>{counter.value}</p>
+    </div>
+  );
+}
+
+export default App;
