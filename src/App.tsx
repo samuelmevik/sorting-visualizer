2ca465cbb8f425aaeed093ba8c243a125ed0a8c2
samuelmevik <samuel.mevik@outlook.com>
2024-03-02 15:35:59 +0100

    Visualizer comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Information from "./components/Information";
+import Visualizer from "./components/Visualizer";
 
 function App() {
   return (
-    <div className="mx-auto flex max-w-3xl flex-col">
+    <div className="mx-auto flex h-screen max-w-3xl flex-col">
       <Header />
       <Information />
+      <Visualizer />
     </div>
   );
 }
---
+++ src/components/Visualizer.tsx
@@ -0,0 +1,9 @@
+function Visualizer() {
+  return (
+    <div className="grow bg-red-700">
+      <h1>Visualizer</h1>
+    </div>
+  );
+}
+
+export default Visualizer;
