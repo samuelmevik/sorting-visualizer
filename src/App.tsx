e2f9ea3ed6140dc93b2d4dba0f42fec9483bf28b
samuelmevik <samuel.mevik@outlook.com>
2024-03-02 14:01:48 +0100

    Use header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { signal } from "@preact/signals-react";
+import Header from "./components/Header";
 
-const counter = signal<number>(0);
 function App() {
   return (
     <div className="mx-auto flex max-w-3xl flex-col">
-      <h1 className="bg-blue-200">Counter</h1>
-      <button onClick={() => (counter.value += 1)}>Increment</button>
-      <button onClick={() => (counter.value -= 1)}>Decrement</button>
-      <p>{counter.value}</p>
+      <Header />
     </div>
   );
 }
