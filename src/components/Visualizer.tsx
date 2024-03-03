df302708072c58b6cc9bcf88263ebdac33e7da3e
samuelmevik <samuel.mevik@outlook.com>
2024-03-03 11:51:55 +0100

    renders bars

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -1,7 +1,20 @@
+import Bar from "./Bar";
+import { signal } from "@preact/signals-react";
+
+const numberOfBars = 100;
+
+export const heights = Array.from({ length: numberOfBars }, () =>
+  signal(Math.floor(Math.random() * 100)),
+);
+
 function Visualizer() {
   return (
-    <div className="grow bg-red-700">
-      <h1>Visualizer</h1>
+    <div className="grow">
+      <div className="flex h-full items-end justify-between bg-red-700">
+        {heights.map((heightSignal, index) => (
+          <Bar key={index} height={heightSignal} />
+        ))}
+      </div>
     </div>
   );
 }
