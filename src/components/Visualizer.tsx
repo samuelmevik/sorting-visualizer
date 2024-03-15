dad459c2e74eca893218fb1602e24d468bd99e43
samuelmevik <samuel.mevik@outlook.com>
2024-03-15 10:00:27 +0100

    Misc: Rendering technique Using signals for rendering

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -1,18 +1,20 @@
+import { FrameType } from "../algorithms/types";
 import Bar from "./Bar";
-import { signal } from "@preact/signals-react";
+import { Signal, signal } from "@preact/signals-react";
 
 const numberOfBars = 100;
 
-export const heights = Array.from({ length: numberOfBars }, () =>
-  signal(Math.floor(Math.random() * 100)),
-);
+export const bars: Signal<{ height: number; frameType: FrameType }>[] =
+  Array.from({ length: numberOfBars }, () =>
+    signal({ height: Math.floor(Math.random() * 100), frameType: "default" })
+  );
 
 function Visualizer() {
   return (
     <div className="grow">
-      <div className="flex h-full items-end justify-between bg-red-700">
-        {heights.map((heightSignal, index) => (
-          <Bar key={index} height={heightSignal} />
+      <div className="flex h-full items-end justify-between">
+        {bars.map((signal, index) => (
+          <Bar key={index} signal={signal} />
         ))}
       </div>
     </div>
