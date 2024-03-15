96b31b88dd00b681cfd8964c14bfc9574c789828
samuelmevik <samuel.mevik@outlook.com>
2024-03-15 09:57:30 +0100

    Misc: Height state The height is now stored using a signal

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,10 +1,28 @@
 import { Signal } from "@preact/signals-react";
+import { FrameType } from "../algorithms/types";
 
-function Bar({ height }: { height: Signal<number> }) {
+function Bar({
+  signal,
+}: {
+  signal: Signal<{ height: number; frameType: FrameType }>;
+}) {
+  const color = () => {
+    switch (signal.value.frameType) {
+      case "comparing":
+        return "bg-green-500";
+      case "swapping":
+        return "bg-red-500";
+      case "sorted":
+        return "bg-yellow-500";
+      default:
+        return "bg-blue-500";
+    }
+  };
+
   return (
     <div
-      className="w-1 rounded-t-full bg-blue-500 transition-colors duration-[10ms]"
-      style={{ height: `${height.value}%` }}
+      className={`w-1 rounded-t-full transition-colors duration-[10ms] ${color()}`}
+      style={{ height: `${signal.value.height}%` }}
     ></div>
   );
 }
